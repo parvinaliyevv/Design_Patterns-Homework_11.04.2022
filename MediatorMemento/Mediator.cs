8539c9066d25c5b976b528d535ab224af0510015
parvinaliyevv <aliyevv_p@outlook.com>
2022-04-13 05:50:33 +0400

    modified

---
+++ MediatorMemento/Mediator.cs
@@ -0,0 +1,82 @@
+﻿namespace MediatorMemento;
+
+public interface IMediator
+{
+    void Notify(object sender, EventArgs e);
+}
+
+public class AuthenticationMediator : IMediator
+{
+    private string Title { get; set; }
+    private bool LoginOrRegister { get; set; }
+
+    private TextBox LoginUsername { get; set; }
+    private TextBox LoginPassword { get; set; }
+
+    private TextBox RegUsername { get; set; }
+    private TextBox RegPassword { get; set; }
+    private TextBox RegEmail { get; set; }
+
+    private Button Next { get; set; }
+    private Button Cancel { get; set; }
+
+    private CheckBox RememberMe { get; set; }
+
+
+    public AuthenticationMediator()
+    {
+        Title = "Authentication";
+        LoginOrRegister = false;
+
+        LoginUsername = new(this);
+        LoginPassword = new(this);
+
+        RegUsername = new(this);
+        RegPassword = new(this);
+        RegEmail = new(this);
+
+        Next = new(this);
+        Cancel = new(this);
+
+        RememberMe = new(this);
+    }
+
+
+    public void Notify(object sender, EventArgs e)
+    {
+        if (sender == Next) { }
+        else if (sender == Cancel) { }
+    }
+}
+
+
+public abstract class UIElement { }
+public abstract class Component : UIElement
+{
+    private IMediator _mediator { get; set; }
+
+    protected Component(IMediator dialog) => _mediator = dialog;
+
+    public void Click() => _mediator.Notify(this, new EventArgs());
+}
+public abstract class ButtonBase : Component
+{
+    protected ButtonBase(IMediator dialog) : base(dialog) { }
+}
+public abstract class TextBoxBase : Component
+{
+    protected TextBoxBase(IMediator dialog) : base(dialog) { }
+}
+
+public class Button : ButtonBase
+{
+    public Button(IMediator dialog) : base(dialog) { }
+}
+public class CheckBox : ButtonBase
+{
+    public CheckBox(IMediator dialog) : base(dialog) { }
+}
+public class TextBox : TextBoxBase
+{
+    public TextBox(IMediator dialog) : base(dialog) { }
+}
---
+++ MediatorMemento/Memento.cs
@@ -0,0 +1,75 @@
+﻿namespace MediatorMemento;
+
+public interface ISnapshoot
+{
+    void Restore();
+}
+
+public class TextEditor
+{
+    public class Snapshoot: ISnapshoot
+    {
+        private readonly TextEditor _originator;
+
+
+        private readonly StringBuilder _text;
+
+        private readonly ConsoleColor _color;
+
+        private readonly int _cursorPosition;
+
+
+        public Snapshoot(TextEditor originator, StringBuilder text, ConsoleColor color, int cursorPosition)
+        {
+            _originator = originator;
+
+            _text = new(text.ToString());
+            _color = color;
+            _cursorPosition = cursorPosition;
+        }
+
+
+        public void Restore()
+        {
+            _originator._Text = new(_text.ToString());
+            _originator._Color = _color;
+            _originator._CursorPosition = _cursorPosition;
+        }
+    }
+
+
+    private StringBuilder _Text { get; set; }
+
+    private ConsoleColor _Color { get; set; }
+
+    private int _CursorPosition { get; set; }
+
+
+    public TextEditor(ConsoleColor color)
+    {
+        _Text = new(string.Empty);
+        _Color = color;
+        _CursorPosition = default;
+    }
+
+
+    public Snapshoot MakeSnapshoot() => new Snapshoot(this, _Text, _Color, _CursorPosition);
+}
+
+public class CareTaker
+{
+    private Stack<ISnapshoot> _mementos;
+
+    public TextEditor Originator { get; set; }
+
+
+    public CareTaker()
+    {
+        Originator = new(ConsoleColor.DarkCyan);
+        _mementos = new();
+    }
+
+
+    public void Undo() => _mementos.Pop()?.Restore();
+    public void MakeSnapshoot() => _mementos.Push(Originator.MakeSnapshoot());
+}
